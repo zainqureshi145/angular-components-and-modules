adf5230847ebf21598eb47ae783b566040f7d701
Zain Qureshi <zainqureshi145@gmail.com>
2021-03-18 18:49:10 +0500

    lazy loading, new module, widget and <ng-content>

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [ElementsHomeComponent],
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ],
   //exports: [ElementsHomeComponent]
 })
