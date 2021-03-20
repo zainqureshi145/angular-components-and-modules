1acae32b756ba1623fbb76343a79a7f198a0932c
Zain Qureshi <zainqureshi145@gmail.com>
2021-03-20 20:39:20 +0500

    Modal issues

--- src/app/mods/mods-routing.module.ts
+++ src/app/mods/mods-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModalComponent } from './modal/modal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ModalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
